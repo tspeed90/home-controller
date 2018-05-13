e3847bb611383506cdf1271b302e09ab35bf4127
tspeed90 <tspeed90@gmail.com>
2018-05-13 20:56:30 +0100

    adds auth route and redirects home

--- server.js
+++ server.js
@@ -5,19 +5,27 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-const options = {
-  cert: fs.readFileSync('./sslcert/fullchain.pem'),
-  key: fs.readFileSync('./sslcert/privkey.pem')
-};
 
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);
 });
-https.createServer(options, app).listen(8443);
+
+if (fs.existsSync('./sslcert/fullchain.pem', () => {
+  const options = {
+    cert: fs.readFileSync('./sslcert/fullchain.pem'),
+    key: fs.readFileSync('./sslcert/privkey.pem')
+  };
+  https.createServer(options, app).listen(8443);
+}));
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>');
 });
 
-app.use(express.static('public'));
+app.get('/auth', (req, res) => {
+  console.log('at auth!');
+  res.redirect(302, '/');
+  res.end();
+});
 
+app.use(express.static('public'));
