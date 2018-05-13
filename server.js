4d14318386de6545306ed28f5c5ed582e3dc6f4b
tspeed90 <tspeed90@gmail.com>
2018-05-13 21:51:27 +0100

    adds token route for authorization

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const authCode = 'fdasjafluudsadj54qrwqfafda$';
+const token = 'fdajklfdajkt5egdadsafgg';
 
 
 app.listen(port, () => {
@@ -24,11 +25,26 @@ app.get('/', (req, res) => {
 });
 
 app.get('/auth', (req, res) => {
-  console.log(req.query);
   const redirectUrl = req.query.redirect_uri;
   const state = req.query.state;
   res.redirect(`${redirectUrl}?code=${authCode}&state=${state}`);
   res.end();
+});
+
+app.get('/token', (req, res) => {
+  const code = req.query.code;
+  if (code !== authCode) {
+    res.status(400);
+    return res.send('Invalid Code');
+  }
+
+  res.status(200).json({
+    token_type: 'bearer',
+    access_token: token,
+    refresh_token: token,
+  });
+
+
 });
 
 app.use(express.static('public'));
