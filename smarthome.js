70e0f2cc0f29dc3695b1ed0aa421e5e2d206a3aa
tspeed90 <tspeed90@gmail.com>
2018-06-04 21:46:02 +0100

    adds onSync method to synchronize devices

--- smarthome.js
+++ smarthome.js
@@ -4,4 +4,24 @@ const homeApp = smarthome({
   key: ''
 });
 
+homeApp.onSync(body => {
+  //need to pass the requestID back in response
+  const response = {
+    requestId: req.body.requestId,
+    payload: {
+      devices: [
+        {
+          id: 'lights',
+          type: 'action.devices.types.LIGHT',
+          traits: ['action.devices.traits.OnOff'],
+          name: {
+            name: 'lights'
+          },
+          willReportState: false
+        }
+      ]
+    }
+  };
+});
+
 module.exports = homeApp;
